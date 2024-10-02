ab64eb520c12ff5fbbd8751884d77ccef5432525
mjy10207 <1020mjy@naver.com>
2024-10-02 11:21:17 +0900

    스프링부트 초기 세팅

---
+++ src/main/java/org/test/teamproject_back/TeamprojectBackApplication.java
@@ -0,0 +1,13 @@
+package org.test.teamproject_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamprojectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/test/teamproject_back/TeamprojectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.teamproject_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
