ded2a58047c0a356af384a981a025132700f4ba6
kim0705 <dbwls138@naver.com>
2024-10-23 16:45:59 +0900

    리뷰 등록 기능 추가

---
+++ src/main/java/org/test/teamproject_back/dto/request/ReqReviewDto.java
@@ -0,0 +1,24 @@
+package org.test.teamproject_back.dto.request;
+
+import lombok.Builder;
+import lombok.Data;
+import org.test.teamproject_back.entity.Review;
+
+@Builder
+@Data
+public class ReqReviewDto {
+    private Long productId;
+    private int rating;
+    private String title;
+    private String content;
+
+    public Review toReview(Long userId) {
+        return Review.builder()
+                .userId(userId)
+                .productId(productId)
+                .rating(rating)
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
