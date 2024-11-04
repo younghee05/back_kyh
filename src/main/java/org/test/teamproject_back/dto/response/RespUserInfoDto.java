5df594b2504aec3b24a32ec16000b8ae12bd3c66
kim0705 <dbwls138@naver.com>
2024-11-04 10:43:36 +0900

    마이페이지 유저 조회 수정

--- src/main/java/org/test/teamproject_back/dto/response/RespUserInfoDto.java
+++ src/main/java/org/test/teamproject_back/dto/response/RespUserInfoDto.java
@@ -14,6 +14,7 @@ public class RespUserInfoDto {
     private String email;
     private String phoneNumber;
     private String password;
+    private Long point;
     private String img;
     private Address address;
 }
